d9b14e4be145e262327e444361990b92b9da79a7
raytchellee <amakaoforkaja@gmail.com>
2023-04-21 18:37:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all the hexadecimal numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	char alph = 'a';
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	while (alph <= 'f')
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}
