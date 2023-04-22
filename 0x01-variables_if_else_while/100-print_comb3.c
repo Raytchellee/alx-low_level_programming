fba237dab7c4ab54bc4333184eceff71766b65b3
raytchellee <amakaoforkaja@gmail.com>
2023-04-21 19:03:56 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints comma separated single numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+
+	while (i <= 57)
+	{
+		j = 48;
+		while (j <= 57)
+		{
+			if(i < j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+			}
+			j++;			
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
