0c86ca9cb2e730f016e67f948b2710618a88067c
raytchellee <amakaoforkaja@gmail.com>
2023-04-25 09:21:43 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_islower - check if an alphabet is lowercase the output
+ *Return: return 1 on success
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
