e431f8d13275caed17a5aec80a4c88246890550f
raytchellee <amakaoforkaja@gmail.com>
2023-04-25 04:47:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - prints _putchar to the output
+ *
+ */
+
+int main()
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	for(i = 0; i < 8; i++)
+	{
+		_putchar("%c",str[i]);
+	}
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+/**
+ *_putchar - writes a character to the output
+ *
+ */
+
+
+_putchar(char);
