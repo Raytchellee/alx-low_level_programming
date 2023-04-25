58c1ded4f29b66ae7f2706e8dee5341ff40f3563
raytchellee <amakaoforkaja@gmail.com>
2023-04-25 12:33:23 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the time table.
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+int i, j;
+
+i = 0;
+j = 0;
+while (i < 10)
+{
+	j = 0;
+	while (j < 10)
+	{
+		int sum;
+		sum = i * j;
+		if (sum >= 10)
+		{
+			_putchar((sum / 10) + '0');
+		}
+		_putchar((sum % 10) + '0');
+		_putchar(',');
+		if (j == 9)
+		{
+			_putchar('\n');
+			continue;
+		}
+		_putchar(' ');
+		j++;
+	}
+	i++;
+}
+}
