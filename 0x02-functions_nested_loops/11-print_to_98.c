20fc1d5fc596f99411348f509fdba9eea22b61ed
raytchellee <amakaoforkaja@gmail.com>
2023-04-25 13:10:24 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,16 +15,16 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			printf("%d, ", n);
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
 		for (i = n; i > 98; i--)
 		{
-			printf("%d, ", n);
+			printf("%d, ", i);
 		}
 	
 	}
-	printf("%d", 98);
+	printf("%d ", 98);
 }
