b54d66651eab48803a4d6b75c02a13bb0514bb9c
raytchellee <amakaoforkaja@gmail.com>
2023-04-27 04:48:41 -0700

    updated file

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,13 +3,11 @@
 /**
  *mul - returns product of two numbers
  *@a: input to check
- *@b: input to check
+ *@b: input 2 to check
  *Return: return product
  */
 
 int mul(int a, int b)
 {
-	int product;
-	product = a *b;
-	return (product);
+	return (a *b);
 }
