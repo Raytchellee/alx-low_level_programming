ca2fd4c8c84af60dd41596328dc0621083d22a8c
raytchellee <amakaoforkaja@gmail.com>
2023-04-27 12:56:54 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - prints square lines
+ */
+
+void main(void)
+{
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+
+	printf("%d ", 1);
+	for (i = 2; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" %s%s", fizz, buzz);
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" %s", fizz);
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" %s", buzz);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+}
