5e5d245131aa5b1ad855582a3e9b634163fa1fbf
raytchellee <amakaoforkaja@gmail.com>
2023-04-27 05:05:53 -0700

    updated file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+/**
+ *print_most_numbers - prints all natural numbers except 2 and 4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+	for (num = 48; num <= 57; num++)
+	{
+		if (num != 50 && num != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
