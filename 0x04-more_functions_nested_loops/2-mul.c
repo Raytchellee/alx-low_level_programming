d54fcc98fc7272741f2cc88d5ba029c1991d39ef
raytchellee <amakaoforkaja@gmail.com>
2023-04-27 04:45:47 -0700

    updated file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - returns product of two numbers
+ *@a: input to check
+ *@b: input to check
+ *Return: return product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+	product = a *b;
+	return (product);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@
 int _isupper(int c);
 int _putchar(char);
 int _isdigit(int c);
+int mul(int a, int b);
 
 
 #endif /* MAIN_H */
