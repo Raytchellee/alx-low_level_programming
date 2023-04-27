5eb3311c5e1e75369929f900f79f2e1064eb69ba
raytchellee <amakaoforkaja@gmail.com>
2023-04-27 04:40:23 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks if an input is a number
+ *@c: input to check
+ *Return: return 1 if true and 0 if false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
