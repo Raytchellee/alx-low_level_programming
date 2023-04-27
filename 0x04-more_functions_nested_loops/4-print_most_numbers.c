c9f1a7fc9b3b8cfeb4353dcc97d93969d8c9f696
raytchellee <amakaoforkaja@gmail.com>
2023-04-27 05:09:01 -0700

    updated file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *print_most_numbers - prints all natural numbers except 2 and 4
+ *print_most_numbers - prints numbers except 2 and 4
  *
  */
 
 void print_most_numbers(void)
 {
 	int num;
+
 	for (num = 48; num <= 57; num++)
 	{
 		if (num != 50 && num != 52)
-			_putchar(i);
+			_putchar(num);
 	}
 	_putchar('\n');
 }
