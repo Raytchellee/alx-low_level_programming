1aa3ba98a9b228803beed60e45cddad50df355ab
raytchellee <amakaoforkaja@gmail.com>
2023-04-27 04:57:06 -0700

    3-print_numbers.c file added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints all natural numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
