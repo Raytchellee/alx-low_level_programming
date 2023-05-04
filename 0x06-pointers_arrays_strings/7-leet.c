d1a2cd79a5c0ff218396616d8facd335ba5c9827
raytchellee <amakaoforkaja@gmail.com>
2023-05-04 16:13:58 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,23 @@
 
 char *leet(char *s)
 {
-	int idx = 0, idy;
-	int upper[] = {97, 101, 111, 116, 108};
-	int lower[] = {65, 69, 79, 84, 76};
+	int idx = 0, idy = 0;
+	int upper_case_l[] = {97, 101, 111, 116, 108};
+	int lower_case_l[] = {65, 69, 79, 84, 76};
 	int replacements[] = {4, 3, 0, 7, 1};
 
 	while (*(s + idx) != '\0')
 	{
 		for (idy = 0; idy < 5; idy++)
 		{
-			if (*(s + idx) == upper[i] || *(s + idx) == lower[i])
+			if (*(s + idx) == upper_case_l[idy] || *(s + idx) == lower_case_l[idy])
 			{
-				*(s + idx) = replacements[i];
+				*(s + idx) = 48 + replacements[idy];
 				break;
 			}
 		}
 		idx++;
 	}
+
 	return (s);
 }
