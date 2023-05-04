deeead06edb91bd18e0672adf70c5b1da1784b1e
raytchellee <amakaoforkaja@gmail.com>
2023-05-04 15:29:04 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,24 +8,27 @@
 
 char *cap_string(char *s)
 {
-	int *seperators[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
-	int idx = 0, separator_idx = 0;
+	int idx = 0, separator_idx;
+	int seperators[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
-	if (s[idx] > 96 && s[idx] < 123)
-		s[idx] = s[idx] - 32;
-	idx++;
-
-	while (s[idx] != '\0')
+	if (*(s + idx) > 96 && *(s + idx)< 123)
 	{
-		while (separator_idx < 13)
+		*(s + idx) = *(s + idx)- 32;
+		idx++;
+	}	
+
+	while (*(s + idx) != '\0')
+	{
+		for (separator_idx = 0; separator_idx < 13; separator_idx++)
 		{
-			if (s[idx] == seperators[separator_idx])
+			if (*(s + idx) == seperators[separator_idx])
 			{
-				if ((s[idx + 1] > 96) && (s[idx + 1] < 123))
-					s[idx + 1] = s[idx + 1] - 32;
-				break;
+				if (*(s + (idx + 1)) > 96 && *(s + (idx + 1)) < 123)
+				{
+					*(s + (idx + 1)) = *(s + (idx + 1)) - 32;
+				}
+			break;
 			}
-			separator_idx++;
 		}
 		idx++;
 	}
