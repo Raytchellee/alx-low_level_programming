482686894848b75f4abdf32fcd52e5663699c814
raytchellee <amakaoforkaja@gmail.com>
2023-05-10 05:11:04 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - returns square of a number
+ *@x: input number
+ *@y: input number
+ *Return: square of a number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y < 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * factorial(x, y - 1));
+	}
+}
