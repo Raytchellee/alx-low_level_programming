77db2efae976eaee92e5ce7b883c836d75eafac8
raytchellee <amakaoforkaja@gmail.com>
2023-05-10 04:49:28 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,13 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_puts_recursion(s + 1);
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
