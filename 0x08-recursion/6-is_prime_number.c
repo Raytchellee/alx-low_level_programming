ed6781b8ab946ee55eecb57867d9823032e207e5
raytchellee <amakaoforkaja@gmail.com>
2023-05-10 14:28:57 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ *checkPrime - checks for prime number
+ *@n: input number
+ *@idx: input number
+ *Return: returns 0 or 1
+ */
+
+int checkPrime(int n, int idx)
+{
+	if (idx == n)
+	{
+		return (1);
+	}
+	else if (n % idx == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0 + checkPrime(n, idx + 1));
+	}
+}
+
+/**
+ *is_prime_number - checks for prime number
+ *@n: input number
+ *Return: returns 0 or 1
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+
+	if (n <= 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0 + checkPrime(n, 2));
+	}
+}
