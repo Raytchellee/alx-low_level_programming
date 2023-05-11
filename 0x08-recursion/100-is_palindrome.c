35aee200617ea9c8b2ad2dcad199c0ce49e8f54c
raytchellee <amakaoforkaja@gmail.com>
2023-05-11 01:55:05 -0700

    upated main.h and added 100-is_palindrome.c file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check for palindrome string
+ * @s: input string
+ * @idx: left index
+ * @idy: right index
+ * Return: 0 if false 1 if true
+ */
+
+int checkPalindrome(char *s, int idx, int idy)
+{
+    if (s[idx] == ' '){
+        return 0 + checkPalindrome(s, idx + 1, idy);
+    }
+    if (s[idy] == ' '){
+        return 0 + checkPalindrome(s, idx, idy - 1);
+    }
+    if (idx > idy)
+    {
+        return (1);
+    }
+    else if (s[idx] != s[idy])
+    {
+        return (0);
+    }
+    else if (s[idx] == s[idy])
+    {
+        return 0 + checkPalindrome(s, idx + 1, idy - 1);
+    }
+}
+
+/**
+ * getLength - check for length of string
+ * @s: input string
+ * @idy: input index
+ * Return: length.
+ */
+
+int getLength(char *s, int idy){
+    if (s[idy] != '\0')
+    {
+       getLength(s, idy + 1);
+    }
+    else
+    {
+        return (idy);
+    }
+}
+
+
+/**
+ * is_palindrome - check for palindrome string
+ * @s: input string
+ * Return: 0 if false 1 if true.
+ */
+
+int is_palindrome(char *s)
+{
+    int idx = 0, idy = 0;
+
+    idy = getLength(s, 0);
+
+    return 0 + checkPalindrome(s, idx, idy - 1);
+}
