1a226cf5ad189d4b65082cea2f8e229c181ef540
raytchellee <amakaoforkaja@gmail.com>
2023-05-12 16:11:30 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,17 @@
 int main(int argc, char *argv[])
 {
 	int product;
+
 	if (argc >= 3)
 	{
 		printf("Error\n");
-		return (0);
+		return (1);
 	}
 
 	/*
 	 *atoi - converts numbers to strings
 	 */
-	
+
 	product = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", product);
 	return (0);
