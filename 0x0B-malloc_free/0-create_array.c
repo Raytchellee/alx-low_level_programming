e51ec7535fa594042a9a2afd97efd654a0d95c2f
raytchellee <amakaoforkaja@gmail.com>
2023-05-15 09:31:09 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array and initializes it with a specific char
+ * @size: input integer
+ * @c: input string
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c);
+{
+	unsigned int idx;
+	char *s;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	s = malloc(size * sizeof(char))
+	if (s == 0)
+		return (0);
+
+	for (idx = 0; idx < size; idx++)
+	{
+		s[idx] = c;
+	}
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+A directory for dynamic allocation assessments in C
