49334d32e7da9657a99255d93d667b1003d6b09e
raytchellee <amakaoforkaja@gmail.com>
2023-05-16 14:09:35 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *free_grid - frees a two dimensional array
+ *@grid: input array
+ *@height: input number
+ *Return: none
+ */
+
+void free_grid(int **grid, int height)
+{
+	int idx;
+
+	if (grid != NULL || height > 0)
+	{
+		for (idx = 0; idx < height; idx++)
+		{
+			free(arr[idx]);
+		}
+		free(arr);
+	}
+
+}
