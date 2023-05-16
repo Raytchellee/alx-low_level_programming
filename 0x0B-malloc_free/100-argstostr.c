a271838d288cc899783a2bc8a619063b59cbeba6
raytchellee <amakaoforkaja@gmail.com>
2023-05-16 15:05:25 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *argstostr - concatenates string
+ *@av: input array
+ *@ac: input number
+ *Return: string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int idx, idy, idz = 0, len = 0;
+	char *s;
+
+	if (av == NULL || ac <= 0)
+	{
+		return (NULL);
+	}
+
+	for (idx = 0; idx < ac; idx++)
+	{
+		for (idy = 0; av[idx][idy] != '\0'; idy++)
+		{
+			len++;
+		}
+	}
+	s = (char *)malloc((len + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (idx = 0; idx < ac; idx++)
+	{
+		for (idy = 0; av[idx][idy] != '\0'; idy++)
+		{
+			s[idz] = av[idx][idy];
+			idz++;
+		}
+		s[idz] = '\n';
+		idz++;
+	}
+
+	return (s);
+}
