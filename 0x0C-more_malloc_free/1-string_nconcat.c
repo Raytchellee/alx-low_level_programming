1d7633953ae8882900389dabffa8e716d6f2ed78
raytchellee <amakaoforkaja@gmail.com>
2023-05-18 10:58:25 -0700

    check code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		l1++;
 		len++;
 	}
-	while (*(s2 + l2) != '\0' && l2 < n)
+	while (*(s2 + l2) != '\0' && l2 < idy)
 	{
 		*(s + len) = *(s2 + l2);
 		l2++;
