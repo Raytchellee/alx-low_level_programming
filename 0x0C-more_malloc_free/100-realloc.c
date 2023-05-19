09987777b7151cc794535ae73c3592dae780801b
raytchellee <amakaoforkaja@gmail.com>
2023-05-19 06:58:21 -0700

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - returns pointer
+ * @old_size: input int
+ * @new_size: input int
+ * @ptr: input pointer
+ * Return: memory array
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (old_size == new_size)
+		return (ptr);
+
+	if (new_size == 0)
+	{
+		if (ptr != NULL)
+			free(ptr);
+		return (NULL);
+	}
+
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
+}
