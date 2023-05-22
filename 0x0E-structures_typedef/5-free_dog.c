e4d79c4e1f262fc4ca5a9fd24d2a081f1f341b80
raytchellee <amakaoforkaja@gmail.com>
2023-05-22 16:33:02 -0700

    free the dog!!!

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free the poor dog.
+ * @d: variable of type dog
+ * Return: none
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
