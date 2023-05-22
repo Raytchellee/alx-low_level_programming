821bd082b624f4feceab89a1c7ecedab7dd59572
raytchellee <amakaoforkaja@gmail.com>
2023-05-22 15:46:49 -0700

    added a C ile and updated the header file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - initializes variable d below.
+ * @d: variable of type dog
+ * @name: name of dog.
+ * @age: dog age.
+ * @owner: owner of dog.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
