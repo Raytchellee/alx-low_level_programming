a1e098fa246b35696857e95e305dec0379e221c2
raytchellee <amakaoforkaja@gmail.com>
2023-05-24 15:10:59 -0700

    array iterator test

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates array and executes action on each index
+ * @array: input array.
+ * @size: array size.
+ * @action: function pointer.
+ * Return: none.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int idx;
+
+	if (action && size)
+	{
+		for (idx = 0; idx < size; idx++)
+			action(array[idx]);
+	}	
+}
