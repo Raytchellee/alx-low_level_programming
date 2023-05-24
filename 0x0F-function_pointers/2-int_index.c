b29b56d9e0dbbb2146634341f2fe8dcd9db01597
raytchellee <amakaoforkaja@gmail.com>
2023-05-24 15:42:32 -0700

    Find index please work!

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: input array.
+ * @size: array size.
+ * @cmp: compare function.
+ * Return: index.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int idx;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	if (array && cmp)
+	{
+		for (idx = 0; idx < size; idx++)
+		{
+			if (cmp(array[idx]))
+			{
+				return (idx);
+			}
+		}
+	}
+
+	return (-1);
+}
