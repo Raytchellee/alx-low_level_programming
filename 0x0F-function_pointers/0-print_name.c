201d8cdc6978bae9eb730fab76348f075d28e218
raytchellee <amakaoforkaja@gmail.com>
2023-05-24 14:50:51 -0700

    print name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+
+/**
+ * print_name - prints name
+ * @name: input string
+ * @f: function to use
+ * Return: none
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
