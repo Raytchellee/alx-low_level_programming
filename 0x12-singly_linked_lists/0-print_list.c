9ae044e2d1e2cd881a6450a01e821184810837d4
raytchellee <amakaoforkaja@gmail.com>
2023-06-01 08:16:44 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints list_t elements
+ * @h: linked list.
+ * Return: total element in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+        {
+			printf("[%i] %s\n", 0, "(nil)");
+        }
+		else
+        {
+			printf("[%i] %s\n", h->len, h->str);
+        }
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
