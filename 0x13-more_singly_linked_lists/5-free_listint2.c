ef0cf78774132ee099ba4a9b74666fdea3ba1572
raytchellee <amakaoforkaja@gmail.com>
2023-06-06 14:08:41 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees linked list sets head to NULL
+ * @head: head of linked list
+ * Return: none
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node, current_node;
+
+	if (head != NULL)
+	{
+		current_node = *head;
+		while (current_node != NULL){
+			temp_node = current_node;
+			current_node = current_node->next;
+			free(temp_node);
+		}
+		
+		*head = NULL;
+	}
+}
