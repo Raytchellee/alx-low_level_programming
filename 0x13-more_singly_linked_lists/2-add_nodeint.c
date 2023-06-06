924a5535f9f226c9499792cbcc15428b49d883a3
raytchellee <amakaoforkaja@gmail.com>
2023-06-06 13:49:48 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,11 +15,11 @@ listint_t *add_nodeint(listint_t **head, int n)
 
 	if (!new_node)
 		return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
-	
+
 	return (*head);
 }
 
