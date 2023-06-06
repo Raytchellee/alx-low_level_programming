8c67239398dfd22ef4874158c703bf64644be045
raytchellee <amakaoforkaja@gmail.com>
2023-06-06 14:54:46 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
-	if (temp == NULL)
+	if (temp == NULL && idx != 0)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
