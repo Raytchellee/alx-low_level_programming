69ce0dd57a3e59704762a2a17632fa86886d4e87
raytchellee <amakaoforkaja@gmail.com>
2023-06-08 14:06:29 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+/**
+ * binary_to_uint: converts binary to unsigned integer
+ * @b: binary string to convert
+ * Return: unsigned integer
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+	int idx = 0, multiplier;
+
+	if (b == NULL)
+		return (0);
+	
+	while (b[idx] != '\0')
+	{
+		if (b[idx] != '0' && b[idx] != '1')
+			return (0);
+		idx++;
+	}
+	idx--;
+
+	for (idx, multiplier = 1; idx > -1; idx--, multiplier *= 2)
+	{
+		if (b[idx] == '1')
+		{
+			dec += multiplier;
+		}
+	}
+
+	return (dec);
+}
