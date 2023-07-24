b46d01ef3f706e4a780e5fb3bedca919b455d519
raytchellee <amakaoforkaja@gmail.com>
2023-07-24 09:42:20 -0700

    check 3

---
+++ 0x18-dynamic_libraries/files.c
@@ -0,0 +1,75 @@
+/**
+  * add - adds inputs
+  * @a: input
+  * @b: input
+  * Return: 0
+  */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
+
+/**
+  * sub - subtracts input
+  * @a: input
+  * @b: input
+  * Return: 0
+  */
+
+int sub(int a, int b)
+{
+	int c;
+
+	c = a - b;
+	return (c);
+}
+
+/**
+  * mul - multiplies input
+  * @a: input
+  * @b: input
+  * Return: 0
+  */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
+/**
+  * div - divides input
+  * @a: input
+  * @b: input
+  * Return: 0
+  */
+
+int div(int a, int b)
+{
+	int c;
+
+	c = a / b;
+	return (c);
+}
+
+/**
+  * mod - returns the modulus
+  * @a: input
+  * @b: input
+  * Return: 0
+  */
+
+int mod(int a, int b)
+{
+	int c;
+
+	c = a % b;
+	return (c);
+}
+
