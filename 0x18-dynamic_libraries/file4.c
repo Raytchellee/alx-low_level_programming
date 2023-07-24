529948cba23d93962387d9effe3cebe0f2752f04
raytchellee <amakaoforkaja@gmail.com>
2023-07-24 09:17:56 -0700

    first task check

---
+++ 0x18-dynamic_libraries/file4.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+  * _memcpy - checks for digit
+  * @dest: input
+  * @src: input
+  * @n: input
+  * Return: 0
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (dest);
+}
+
+/**
+  * _strchr - checks for digit
+  * @s: input
+  * @c: input
+  * Return: 0
+  */
+
+char *_strchr(char *s, char c)
+{
+	return (s);
+}
+
+/**
+  * _strspn - checks for digit
+  * @s: input
+  * @accept: input
+  * Return: 0
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+
+	return (i);
+}
+
+/**
+  * _strpbrk - checks for digit
+  * @s: input
+  * @accept: input
+  * Return: 0
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (s);
+}
+
+/**
+  * _strstr - checks for digit
+  * @haystack: input
+  * @needle: input
+  * Return: 0
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (needle);
+}
