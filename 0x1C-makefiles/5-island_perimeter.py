99adfa6e7ef2b41f1f1b0528bbe2d0e6086d4f55
raytchellee <amakaoforkaja@gmail.com>
2023-09-28 05:10:31 -0700

    created new task files and added README file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    perimeter = 0
+
+    for idx in range(0, len(grid)):
+        for idy in range(0, len(grid[idx])):
+            if grid[idx][idy] == 1:
+                perimeter += getPerimeter(idx, idy, grid)
+
+    return perimeter
+
+
+def getPerimeter(idx, idy, grid):
+    count = 0
+    limitx = len(grid)
+    limity = len(grid[idx])
+
+    if idx - 1 >= 0:
+        if (grid[idx - 1][idy] == 0):
+            count += 1
+    if idx + 1 < limitx:
+        if (grid[idx + 1][idy] == 0):
+            count += 1
+    if idy - 1 >= 0:
+        if (grid[idx][idy - 1] == 0):
+            count += 1
+    if idy + 1 < limity:
+        if (grid[idx][idy + 1] == 0):
+            count += 1
+
+    if count != 0 and count != 4:
+        if idx - 1 < 0:
+            count += 1
+        if idx + 1 >= limitx:
+            count += 1
+        if idy - 1 < 0:
+            count += 1
+        if idy + 1 >= limity:
+            count += 1
+
+    if count == 4:
+        return 0
+    else:
+        return count
