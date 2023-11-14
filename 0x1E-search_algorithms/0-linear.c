ed40a8dd894db8e0074904f10f1f86f8c5e86cb9
raytchellee <amakaoforkaja@gmail.com>
2023-11-14 01:55:53 -0800

    task check

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - finds a value in an array linear
+ * @array: array to search
+ * @size: length of array
+ * @value: input value to check for
+ * Return: values or -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t idx = 0;
+
+	if (!array)
+		return (-1);
+	while (idx < size)
+	{
+		printf("Value checked array[%d] = [%d]", idx, array[idx]);
+		if (array[idx] == value)
+		{
+			return (value);
+		}
+		idx++;
+	}
+	return (-1);
+}
