24caa173b8aa04b24aeed95a91dbc784fb3b5e2d
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2023-11-16 00:39:53 +0100

    checking

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,8 @@
 #ifndef SEARCH_ALGO_H
 #define SEARCH_ALGO_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - singly linked list
@@ -48,6 +48,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
 
 #endif /* algorithms */
